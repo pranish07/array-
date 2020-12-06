c55af7aebf21f127c124b367e399a18ca04ff9ab
pranish07 <49307206+pranish07@users.noreply.github.com>
2020-12-06 12:45:30 +0545

    Update array.js

--- array.js
+++ array.js
@@ -1,18 +1,21 @@
-var favoritePlaces= ["Biratnagar", "Lalitpur", "Patan", "Kathmandu","Pokhara","Palpa"];
-console.log("the old best place : " + favoritePlaces);
-{
-    
-var ktmSplice = favoritePlaces.splice(3,1);
-var varOfLength= favoritePlaces.length;
-console.log("The left favorite place :  " + favoritePlaces);
-console.log("The removed place is " + ktmSplice );
-console.log(varOfLength);
- 
+var favoritePlaces = [
+    "Biratnagar",
+    "Lalitpur",
+    "Patan",
+    "Kathmandu",
+    "Pokhara",
+    "Palpa",
+];
+console.log(favoritePlaces);
+
 favoritePlaces.push("Bhairahawa", "Illam");
-console.log("Added two places: " + favoritePlaces)
-}
+var ktmSplice = favoritePlaces.splice(3, 1);
+var varOfLength = favoritePlaces.length;
+console.log(varOfLength);
+
+favoritePlaces.forEach(function (value) {
+    console.log(value);
+
+});
 
 
-// }
-// alert("now myoverall favorite places are " + varOfLength);
-// ("the deleted city from my favorite place is " + ktmSplice + " It's just because of pollution ");a
